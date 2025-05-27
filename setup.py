b299dde6ff7d1f838b5147c3ad9a19b24a2f1255
Sri Krishna <srikrishna4569@gmail.com>
2025-05-27 10:30:04 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Sri Krishna",
+    author_email="srikrishnasrikrishna542@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
